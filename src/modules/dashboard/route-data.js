ed75f8749e326d32139e8b280512c1c1e06aa9cc
mailtodhaval23 <mailto.dhaval23@gmail.com>
2023-11-03 14:44:54 +0530

    Removed unwanted routes. Updated login payload to username instead of employeeCode.

--- src/modules/dashboard/route-data.js
+++ src/modules/dashboard/route-data.js
@@ -37,23 +37,6 @@ export const routeData = (routes) => [
     permissions: true,
     isUserCanView: true, //routes.includes(routeConstant.dashboard)
   },
-  
-    {
-    routeTo: routeConstant.branch,
-    header: "Add/Update Branches",
-    childComp: Branch,
-    data: {},
-    permissions: true,
-    isUserCanView: routes.includes(routeConstant.branch),
-  },
-  {
-    routeTo: routeConstant.department,
-    header: "Add/Update Departments",
-    childComp: Department,
-    data: {},
-    permissions: true,
-    isUserCanView: routes.includes(routeConstant.department),
-  },
   {
     routeTo: routeConstant.roles,
     header: "Add/Update Roles",
@@ -61,14 +44,6 @@ export const routeData = (routes) => [
     data: {},
     permissions: true,
     isUserCanView: routes.includes(routeConstant.roles),
-  },
-  {
-    routeTo: routeConstant.employees,
-    header: "Add/Update Employee Data",
-    childComp: Employees,
-    data: {},
-    permissions: true,
-    isUserCanView: routes.includes(routeConstant.employees),
   },
   {
     routeTo: routeConstant.registerUser,
@@ -112,7 +87,6 @@ export const routeData = (routes) => [
     data: {},
     permissions: true,
     isUserCanView: routes.includes(routeConstant.userMaster),
-
   },
  
   {
