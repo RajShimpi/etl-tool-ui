dbe42e3c57ebb61a52322dbac469b0c9bc914fdf
RajShimpi <rajshimpi8812@gmail.com>
2023-12-05 11:03:12 +0530

    work on datapull to the componet sidebar

--- src/modules/dashboard/drag-drop/components/sidebar/sidebar.js
+++ src/modules/dashboard/drag-drop/components/sidebar/sidebar.js
@@ -1,76 +1,30 @@
-import React from "react";
-export default ({ isSelected, textRef, nodeName, setNodeName }) => {
-  const onDragStart = (event, nodeType, content, img) => {
-    event.dataTransfer.setData("application/reactflow", nodeType);
-    event.dataTransfer.setData("content", content);
-    event.dataTransfer.setData("img", img);
-    event.dataTransfer.effectAllowed = "move";
-  };
+// Sidebar.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Sidebar = () => {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    // Make the API call
+    axios.get('http://localhost:3000/step-type')
+      .then(response => {
+        setApiData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // Empty dependency array ensures the effect runs once on mount
 
   return (
-    <aside>
-      <div className="row">
-        <div className="col-md-12">
-          {/* {isSelected &&
-          {/* {isSelected &&
-            <EditMessage
-              textRef={textRef}
-              nodeName={nodeName}
-              setNodeName={setNodeName}
-            />} */}
-        </div>
-        <div className="col-md-12">
-          <div
-            className="dndnode input "
-            style={{margin:'5px'}}
-            onDragStart={(event) =>
-              onDragStart(event, "node", "Click to Edit message", require("../../../../../assets/Images/Files.png"))
-            }
-            draggable
-          >
-           <img src={require('../../../../../assets/Images/Files.png')} style={{height:"20px"}} alt="File Icon" />
-           <div>Data</div>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-12">
-        <div
-          style={{margin:'5px'}}
-          className="dndnode input m-10"
-          onDragStart={(event) =>
-            onDragStart(event, "node", "Click to Edit message", require("../../../../../assets/Images/file1.png"))
-          }
-          draggable
-        >
-          <img src={require('../../../../../assets/Images/file1.png')} style={{height:"20px"}} alt="File Icon" />
-          <div>Edit</div>
-        </div>
-      </div>
-      <div className="col-md-12">
-        <div
-          style={{margin:'5px'}}
-          className="dndnode input m-10"
-          onDragStart={(event) =>
-            onDragStart(event, "node", "Click to Edit message", require("../../../../../assets/Images/Folders.png"))
-          }
-          draggable
-        >
-          <img src={require('../../../../../assets/Images/Folders.png')} style={{height:"20px"}} alt="File Icon" />
-          <div>Delete</div>
-        </div>
-      </div>
-      {/* <div className="col-md-12">
-        <div
-          style={{margin:'5px'}}
-          className="dndnode input m-10"
-          onDragStart={(event) =>
-            onDragStart(event, "node", "Click to Edit message", require("../../../../../assets/Images/Files-PNG-Clipart.png"))
-          }
-          draggable
-        >
-          <img src={require('../../../../../assets/Images/Files-PNG-Clipart.png')} style={{height:"20px"}} alt="File Icon" />
-        </div>
-      </div> */}
-    </aside>
-  ); 
+    <div className="">
+      <ul>
+        {apiData.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+export default Sidebar;
