307ac38efe1d4d1ebb72a26c9d8d9a2b96562a05
RajShimpi <rajshimpi8812@gmail.com>
2023-12-19 12:42:13 +0530

    work on the ContextManu

--- src/components/ProjectStrucure/ProjectFolder.js
+++ src/components/ProjectStrucure/ProjectFolder.js
@@ -4,64 +4,65 @@ import FolderIcon from '@mui/icons-material/Folder';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 import './project.css';
 import axios from '../../modules/services/axios';
+import ContextMenu from '../ContextMenu';
 
 function FolderDropdown({
   item,
   textColor,
   project_id,
-  onToggleFolder
+  id,
+  onToggleFolder,
+  openContextMenuId,
+  onContextMenuOpen,
+  onContextMenuClose,
 }) {
-
   const [isOpen, setIsOpen] = useState(false);
   const containerRef = useRef(null);
 
-  // const toggleFiles = (index) => {
-  //   const updatedFile = [...item];
-  //   updatedFile[index].isOpen = !updatedFile[index].isOpen;
-  //   setIsOpen(updatedFile);
-  // };
-  // const toggleFile = () => {
-  //   setIsOpen(!isOpen);
-  // }
-  
   return (
     <>
       <div className='folderstyle' ref={containerRef}>
-
         <div
           style={{ textColor }}
           onClick={onToggleFolder}
+          onContextMenu={(e) => {
+            e.preventDefault();
+            onContextMenuOpen(id);
+          }}
           className={`${isOpen ? '' : 'open'}`}
-
         >
           {item.type === 'Folder' && (
             item.isOpen ? <FolderOpenIcon fontSize='small' /> : <FolderIcon fontSize='small' />
           )}
           {item.type === 'File' && <InsertDriveFileIcon fontSize='small' />}
           {item.file_name}
+          {openContextMenuId === id && (
+            <ContextMenu onClose={onContextMenuClose} project_id={project_id} id={id} textColor={textColor} />
+          )}
         </div>
+
         {item.isOpen && item.type === 'Folder' && (
-          <FolderContainer parentId={item.id} projectId={project_id} />
+          <FolderContainer parentId={item.id} projectId={project_id} textColor={textColor} />
         )}
       </div>
     </>
   );
 }
+
 function FolderContainer({ parentId, projectId, textColor }) {
   const [folder, setFolder] = useState([]);
-  const [openContextMenuForItemId, setOpenContextMenuForItemId] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
-
+  const [contextMenuOpenItemId, setContextMenuOpenItemId] = useState(null);
 
   useEffect(() => {
-    let url = parentId ? 'project-files/get-folder-hierarchy?projectId=' + projectId + '&parentId=' + parentId : 'project-files/get-folder-hierarchy?projectId=' + projectId
-    axios.getWithCallback(url, (data) => {
+    let url = parentId
+      ? `project-files/get-folder-hierarchy?projectId=${projectId}&parentId=${parentId}`
+      : `project-files/get-folder-hierarchy?projectId=${projectId}`;
 
+    axios.getWithCallback(url, (data) => {
       setFolder(data);
       console.log(data);
     });
   }, [parentId, projectId]);
-
 
   const toggleFolder = (index) => {
     const updatedFolder = [...folder];
@@ -69,39 +70,33 @@ function FolderContainer({ parentId, projectId, textColor }) {
     setFolder(updatedFolder);
   };
 
-  const handleContextMenu = (item) => {
-    console.log('Right-clicked on:', item);
-    console.log('Project ID:', item.project_id);
-    console.log('Parent ID:', item.parent_id);
-    console.log('Id:', item.id);
-    console.log('File Name:', item.file_name);
-    setOpenContextMenuForItemId(item.id);
+  const handleContextMenuOpen = (itemId) => {
+    setContextMenuOpenItemId(itemId);
+  };
+
+  const handleContextMenuClose = () => {
+    setContextMenuOpenItemId(null);
   };
 
   return (
     <div className="folder-container">
-
       {folder.map((folderItem, index) => (
-        <div className={`${isOpen ? 'open' : ''}`}>
+        <div className={`${folderItem.isOpen ? 'open' : ''}`} key={index}>
           <FolderDropdown
-            key={index}
             item={folderItem}
             project_id={folderItem.project_id}
             parent_id={folderItem.parent_id}
             id={folderItem.id}
-            onContextMenu={(item) => handleContextMenu(item)}
-            openContextMenuForItemId={openContextMenuForItemId}
+            onContextMenuOpen={handleContextMenuOpen}
+            onContextMenuClose={handleContextMenuClose}
+            openContextMenuId={contextMenuOpenItemId}
             textColor={textColor}
             onToggleFolder={() => toggleFolder(index)}
-          // onToggleFolder={ toggleFolder}
           />
-
         </div>
       ))}
-
     </div>
   );
 }
-
 
 export default FolderContainer;
