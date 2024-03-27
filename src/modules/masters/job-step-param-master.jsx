23a618c7eb148e699625cd326764bdfff403417b
RajShimpi8812 <raj.shimpi@abhyeti.in>
2024-03-27 18:07:52 +0530

    fix start step add the update fix

--- src/modules/masters/job-step-param-master.jsx
+++ src/modules/masters/job-step-param-master.jsx
@@ -59,7 +59,7 @@ const JobStepParameterMaster = ({
       setEditName([]);
       setotherParameters([]);
       setparameter([]);
-      // setNameValue([]);
+      setNameValue([]);
       setJobStepParamData([]);
     }
   }, [node_id, nodeid]);
