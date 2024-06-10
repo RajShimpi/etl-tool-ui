76cea2cc70650f91ce75e8e6ffb9e7f5b9ae3487
unknown <pratik>
2024-06-10 17:22:49 +0530

    data

--- src/modules/dashboard/route-data.js
+++ src/modules/dashboard/route-data.js
@@ -94,8 +94,8 @@ export const routeData = (routes) => [
     childComp: AddMenu,
     data: {},
     permissions: true,
-    // isUserCanView: true,
-    isUserCanView: routes.includes(routeConstant.addMenu),
+    isUserCanView: true,
+    // isUserCanView: routes.includes(routeConstant.addMenu),
   },
   {
     routeTo: routeConstant.dashboardBuilder,
